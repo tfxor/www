7b221f5ac20a39d69cfa5d3c9e377b1df6d8569d
Eugene <eistrati@mitocgroup.com>
2019-03-11 20:35:41 -0400

    overwrite aws account id

--- bin/deploy-codepipeline.sh
+++ bin/deploy-codepipeline.sh
@@ -14,5 +14,8 @@ git checkout $BRANCH_TO
 git checkout $BRANCH_FROM
 
 terrahub --version > /dev/null 2>&1 || { echo >&2 "terrahub is missing. aborting..."; exit 1; }
+AWS_ACCOUNT_ID="$(aws sts get-caller-identity --output=text --query='Account')"
+terrahub configure -c template.locals.account_id="${AWS_ACCOUNT_ID}"
+
 terrahub run -y -b ${THUB_APPLY} ${THUB_ENV}
 echo "Execution successful: from ${BRANCH_FROM} into ${BRANCH_TO}"
